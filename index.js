8143383f69893b354762a3b7512174e6b6885d6f
Ryo Kato <ryo_kato@hashedhyphen.com>
2018-08-31 23:18:03 +0900

    Post the original name after getting jobs

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const CLIENT = new Twitter({
 });
 
 const GOT_JOBLESS_AT = new Date(2018, 6, 19, 15); // JST 2018-07-20 0:00
+const GOT_JOB_AT = new Date(2018, 7, 31, 15, 0, 0); // JST 2018-09-01 0:00
+const MAX_DAYS_JOBLESS = (GOT_JOB_AT - GOT_JOBLESS_AT) / 1000 / 60 / 60 / 24;
 const ENDPOINT_FOR_UPDATE_PROFILE = 'account/update_profile';
 
 const calcNthDaySinceJobless = (now = new Date()) => {
@@ -20,7 +22,10 @@ const calcNthDaySinceJobless = (now = new Date()) => {
   return nthDaySinceJobless;
 };
 
-const buildPostParams = ({ nthDay }) => ({ name: `Hash@無職${nthDay}日目` });
+const buildPostParams = ({ nthDay }) => {
+  const name = nthDay <= MAX_DAYS_JOBLESS ? `Hash@無職${nthDay}日目` : 'Hash';
+  return { name };
+};
 
 const postParams = ({ endpoint = ENDPOINT_FOR_UPDATE_PROFILE, params }) =>
   new Promise((resolve, reject) =>
